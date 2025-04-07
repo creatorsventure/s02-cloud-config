fa11fa748893c9877d7d07257dfe60eb41ec3bf2
ram <ram_r@live.com>
2025-04-07 11:59:27 +0400

    BASE

---
+++ src/main/java/com/cv/s02cloudconfig/S02CloudConfigApplication.java
@@ -0,0 +1,15 @@
+package com.cv.s02cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class S02CloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S02CloudConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cv/s02cloudconfig/S02CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.s02cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S02CloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
